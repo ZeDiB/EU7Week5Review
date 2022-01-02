e9f893c8dd6fac1bd12ed27a07483cea6493f470
ZeDiB <zeynepdilara.b@gmail.com>
2022-01-02 16:35:30 +0000

    tast 3 started

--- src/test/java/com/cybertek/tests/ChercherTest.java
+++ src/test/java/com/cybertek/tests/ChercherTest.java
@@ -32,16 +32,6 @@ public class ChercherTest {
     4. Then verify the button is enabled
     */
 
-    /*
-     Task3:
-    1. Go to:  http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
-    2. Login with username: Tester, password: test
-    3. Click  Order button
-    4. Verify under Product Information, selected option is “MyMoney”
-    5. Then select FamilyAlbum, make quantity 2, and click Calculate,
-    6. Then verify Total is equal to Quantity*PricePerUnit
-*/
-
     WebDriver driver;   //declare our reference for the object
     WebDriverWait wait;
 
@@ -93,6 +83,7 @@ public class ChercherTest {
 
 
     }
+
 
 
 }
